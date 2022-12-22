e6fe3c334a984274b7032f2b2a0b771f01b6848f
Jace <livingfern1@live.com>
2022-12-22 08:25:55 -0500

    3.4.0 Update
    3.4.0 Update

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# SpecialSlots
+# Special Slots
+
+### WHAT
+As of version 0.12.12.30.19078, BSG gave us 3 "SPEC" slots. My question is, "Why not allow us to put other items in one of those slots?"
+
+Well, here you go.
+
+Special Slots now hold all.
+
+### Pros/Cons
+
+Pro:
+  * Special Slots can hold anything.
+  * Special Slots can be used to hold containers so you can hold even more items.
+  * Special Slots retain everything after death.
+  * Special Slots don't give a damn.
+
+Cons:
+  * Special Slots cannot hold stacks of objects such as Ammo unless they are in a container.
+  * Special Slots use containers much like the Armor, Backpack, and Secure Container slots so the containers expand when placed in the slots.
+  * Special Slots don't give a damn.
+
+### Good Luck Soldier!
---
+++ src/specSlots.ts
@@ -0,0 +1,27 @@
+/*
+ *      Name: Medical Attention
+ *   Version: 340.0.1
+ * Copyright: jbs4bmx
+ *    Update: 22.12.2022
+*/
+
+import { DependencyContainer } from "tsyringe";
+import { IMod } from "@spt-aki/models/external/mod";
+import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+
+class SpecSlots implements IMod {
+    private pkg;
+    public postDBLoad(container: DependencyContainer): void {
+		const logger = container.resolve<ILogger>("WinstonLogger");
+		const db = container.resolve<DatabaseServer>("DatabaseServer").getTables();
+        const pockets = db.templates.items["627a4e6b255f7527fb05a0f6"];
+        this.pkg = require("../package.json")
+        pockets._props.Slots[0]._props.filters[0].Filter.push("54009119af1c881c07000029");
+        pockets._props.Slots[1]._props.filters[0].Filter.push("54009119af1c881c07000029");
+        pockets._props.Slots[2]._props.filters[0].Filter.push("54009119af1c881c07000029");
+        logger.info(`${this.pkg.author}-${this.pkg.name} v${this.pkg.version}: Cached Successfully`);
+    }
+}
+
+module.exports = { mod: new SpecSlots() }
